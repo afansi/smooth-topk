3b4f6d70d4905187e88873ac1f209d7d6f47f135
Your Name <arsene.fansi@gmail.com>
2020-10-16 15:38:43 -0400

    multi dim handling

--- topk/svm.py
+++ topk/svm.py
@@ -21,7 +21,7 @@ class _SVMLoss(nn.Module):
         self.n_classes = n_classes
         self._tau = None
 
-    def forward(self, x, y):
+    def forward(self, x, y, weight=None, reduction='mean'):
 
         raise NotImplementedError("Forward needs to be re-implemented for each loss")
 
@@ -45,6 +45,15 @@ class _SVMLoss(nn.Module):
         nn.Module.cpu()
         self.get_losses()
         return self
+        
+    def _handle_multi_dim(self, x, y):
+        x_size = None
+        if x.ndim > 2:
+            x = x.transpose(1, -1)
+            x_size = x.size()[:-1]
+            x = x.reshape(-1, x.size(-1))
+            y = y.reshape(-1)
+        return x_size, x, y
 
 
 class MaxTop1SVM(_SVMLoss):
@@ -55,8 +64,21 @@ class MaxTop1SVM(_SVMLoss):
                                          alpha=alpha)
         self.get_losses()
 
-    def forward(self, x, y):
-        return self.F(x, y).mean()
+    def forward(self, x, y, weight=None, reduction='mean'):
+        assert reduction in ('mean', 'sum', 'none')
+        if weight is not None:
+            assert weight.ndim == 1 and weight.size(0) == self.n_classes
+        x_size, x, y = self._handle_multi_dim(x, y)
+        output = self.F(x, y)
+        if weight is not None:
+            output = weight[y] * output
+        if x_size is not None:
+            output = output.reshape(*x_size)
+        if reduction == 'mean':
+            return output.mean()
+        elif reduction == 'sum:
+            return output.sum()
+        return output
 
     def get_losses(self):
         self.F = F.Top1_Hard_SVM(self.labels, self.alpha)
@@ -71,8 +93,21 @@ class MaxTopkSVM(_SVMLoss):
         self.k = k
         self.get_losses()
 
-    def forward(self, x, y):
-        return self.F(x, y).mean()
+    def forward(self, x, y, weight=None, reduction='mean'):
+        assert reduction in ('mean', 'sum', 'none')
+        if weight is not None:
+            assert weight.ndim == 1 and weight.size(0) == self.n_classes
+        x_size, x, y = self._handle_multi_dim(x, y)
+        output = self.F(x, y)
+        if weight is not None:
+            output = weight[y] * output
+        if x_size is not None:
+            output = output.reshape(*x_size)
+        if reduction == 'mean':
+            return output.mean()
+        elif reduction == 'sum:
+            return output.sum()
+        return output
 
     def get_losses(self):
         self.F = F.Topk_Hard_SVM(self.labels, self.k, self.alpha)
@@ -86,20 +121,42 @@ class SmoothTop1SVM(_SVMLoss):
         self.thresh = 1e3
         self.get_losses()
 
-    def forward(self, x, y):
+    def forward(self, x, y, weight=None, reduction='mean'):
+        assert reduction in ('mean', 'sum', 'none')
+        if weight is not None:
+            assert weight.ndim == 1 and weight.size(0) == self.n_classes
+        x_size, x, y = self._handle_multi_dim(x, y)
         smooth, hard = detect_large(x, 1, self.tau, self.thresh)
 
-        loss = 0
+        output = []
         if smooth.data.sum():
             x_s, y_s = x[smooth], y[smooth]
             x_s = x_s.view(-1, x.size(1))
-            loss += self.F_s(x_s, y_s).sum() / x.size(0)
+            out = self.F_s(x_s, y_s)
+            output.append(out)
         if hard.data.sum():
             x_h, y_h = x[hard], y[hard]
             x_h = x_h.view(-1, x.size(1))
-            loss += self.F_h(x_h, y_h).sum() / x.size(0)
+            out = self.F_h(x_h, y_h)
+            output.append(out)
 
-        return loss
+        if len(output) > 1:
+            res = torch.cat(output, dim=0)
+            res[smooth] = output[0]
+            res[hard] = output[1]
+        else:
+            res = output[0]
+
+        output = res
+        if weight is not None:
+            output = weight[y] * output
+        if x_size is not None:
+            output = output.reshape(*x_size)
+        if reduction == 'mean':
+            return output.mean()
+        elif reduction == 'sum:
+            return output.sum()
+        return output
 
     def get_losses(self):
         self.F_h = F.Top1_Hard_SVM(self.labels, self.alpha)
@@ -116,20 +173,42 @@ class SmoothTopkSVM(_SVMLoss):
         self.thresh = 1e3
         self.get_losses()
 
-    def forward(self, x, y):
+    def forward(self, x, y, weight=None, reduction='mean'):
+        assert reduction in ('mean', 'sum', 'none')
+        if weight is not None:
+            assert weight.ndim == 1 and weight.size(0) == self.n_classes
+        x_size, x, y = self._handle_multi_dim(x, y)
         smooth, hard = detect_large(x, self.k, self.tau, self.thresh)
 
-        loss = 0
+        output = []
         if smooth.data.sum():
             x_s, y_s = x[smooth], y[smooth]
             x_s = x_s.view(-1, x.size(1))
-            loss += self.F_s(x_s, y_s).sum() / x.size(0)
+            out = self.F_s(x_s, y_s)
+            output.append(out)
         if hard.data.sum():
             x_h, y_h = x[hard], y[hard]
             x_h = x_h.view(-1, x.size(1))
-            loss += self.F_h(x_h, y_h).sum() / x.size(0)
+            out = self.F_h(x_h, y_h)
+            output.append(out)
 
-        return loss
+        if len(output) > 1:
+            res = torch.cat(output, dim=0)
+            res[smooth] = output[0]
+            res[hard] = output[1]
+        else:
+            res = output[0]
+
+        output = res
+        if weight is not None:
+            output = weight[y] * output
+        if x_size is not None:
+            output = output.reshape(*x_size)
+        if reduction == 'mean':
+            return output.mean()
+        elif reduction == 'sum:
+            return output.sum()
+        return output
 
     def get_losses(self):
         self.F_h = F.Topk_Hard_SVM(self.labels, self.k, self.alpha)
